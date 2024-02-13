ed96e0539b05d3806d6775c5ef9c74fa83524c32
Christian Andersen <chrijand@stud.ntnu.no>
2024-02-13 15:52:38 +0100

    feat: Create Dashboard-page
    - Create dashboard page for viewing your learningsets.
    
    Co-authored-by: kaveeng
    Related to #2

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,10 +3,6 @@ import { useNavigate } from "react-router-dom";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
-import EditIcon from '@mui/icons-material/Edit';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons'; 
-import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons';
 
 interface LearningSet {
   id: number;
@@ -34,11 +30,6 @@ export default function Dashboard() {
     navigate("/create-set");
   };
 
-  const handleEditSets = () => {
-    navigate("/edit-set")
-
-  };
-
   const handleSetFavorite = (id: number) => {
     console.log("Set favorite", id);
   };
@@ -64,13 +55,6 @@ export default function Dashboard() {
         >
           Create new learning set
         </Button>
-        <Button
-          variant="outlined"
-          startIcon={<EditIcon />}
-          onClick={handleEditSets}
-        >
-          Edit learning set
-        </Button>
       </div>
       <Box>
         <Grid container spacing={2} sx={{ overflowY: "scroll" }}>
@@ -88,9 +72,6 @@ export default function Dashboard() {
                   maxWidth: "300px",
                   margin: " auto",
                   position: "relative",
-                  '&:hover': {
-                    transform: 'scale(1.05)', 
-                  }
                 }}
               >
                 <Button
@@ -103,11 +84,9 @@ export default function Dashboard() {
                   }}
                 >
                   {learningSet.isFavorite ? (
-                    // <StarBorderPurple500Icon style={{ color: "gold" }} />
-                    <FontAwesomeIcon icon={fasFaStar} color="gold" />
+                    <StarBorderPurple500Icon style={{ color: "gold" }} />
                   ) : (
-                    // <StarBorderIcon style={{ color: "black" }} />
-                    <FontAwesomeIcon icon={farFaStar} color="black" />
+                    <StarBorderIcon style={{ color: "black" }} />
                   )}
                 </Button>
                 <Typography variant="h6">{learningSet.name}</Typography>
