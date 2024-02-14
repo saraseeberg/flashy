31e9dda13d93ad0c2b514a8f837ff9bf4766cee3
amaliejvik <amaligjo@stud.ntnu.no>
2024-02-14 10:43:10 +0100

    feat: add flip-functionality on cards
    Related to #16

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,21 +1,55 @@
 type CardProps = {
   question: string;
   answer: string;
+  isFlipped: boolean;
 };
 
-const Card: React.FC<CardProps> = ({ question, answer }) => {
+const Card: React.FC<CardProps> = ({ question, answer, isFlipped }) => {
   return (
     <div
       className="card-container"
-      style={{ padding: "2em", background: "pink" }}
+      style={{
+        height: "20em",
+        width: "40em",
+        padding: "2em",
+        display: "flex",
+        justifyContent: "center",
+        background: "#AC94F4",
+        maxWidth: "40em",
+      }}
     >
-      <div className="card">
-        <div className="front">
-          <div className="question">{question}</div>
-        </div>
-        <div className="back">
-          <div className="answer">{answer}</div>
-        </div>
+      <div
+        className="card"
+        style={{
+          maxHeight: "20em",
+          maxWidth: "40em",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {isFlipped ? (
+          <div
+            className="answer"
+            style={{
+              fontSize: "2em",
+              overflowWrap: "break-word",
+              wordBreak: "break-word",
+            }}
+          >
+            {answer}
+          </div>
+        ) : (
+          <div
+            className="question"
+            style={{
+              fontSize: "2em",
+              overflowWrap: "break-word",
+              wordBreak: "break-word",
+            }}
+          >
+            {question}
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/pages/ViewCardsPage.tsx
+++ frontend/src/pages/ViewCardsPage.tsx
@@ -2,35 +2,81 @@ import { Button } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Card from "../components/Card";
+import { useState } from "react";
 
 export default function ViewCards() {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlipButtonClick = () => {
+    setFlipped(!flipped);
+  };
   return (
     <div>
-      <div>
-        <Button>Back</Button>
-      </div>
-      <div>
+      <div style={{ padding: "2em" }}>
         <h2>Title of learningSet</h2>
         <div
           style={{
             display: "flex",
             flexDirection: "column",
+            width: "100%",
             alignItems: "center",
           }}
         >
-          <div style={{ maxWidth: "80%" }}>
+          <div>
             <Card
-              question={"Hva er hovedstaden i Norge?"}
-              answer={"Oslo"}
+              question={"Hvem er den beste PU gruppa?"}
+              answer={"Gruppe 50"}
+              isFlipped={flipped}
             ></Card>
           </div>
-          <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              padding: "1em",
+              maxWidth: "20em",
+            }}
+          >
             <Button>
               <ArrowBackIcon />
             </Button>
-            <Button>Flip</Button>
+            <Button
+              onClick={handleFlipButtonClick}
+              type="button"
+              fullWidth
+              variant="contained"
+              style={{
+                background: "#d3d3d3",
+                opacity: "0.5",
+                color: "black",
+              }}
+            >
+              Flip
+            </Button>
             <Button>
               <ArrowForwardIcon />
+            </Button>
+          </div>
+          <div
+            style={{
+              width: "100%",
+              padding: "1em",
+              maxWidth: "10em",
+            }}
+          >
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              style={{
+                background: "#d3d3d3",
+                maxWidth: "20em",
+                opacity: "0.5",
+                color: "black",
+              }}
+            >
+              Back
             </Button>
           </div>
         </div>
