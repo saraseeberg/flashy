1e9e0c1d1382c091a5df03c9c2544b49319276d6
Christian Andersen <chrijand@stud.ntnu.no>
2024-02-15 16:48:49 +0100

    feat: Implement changes for unknown path
    Related to #2

--- frontend/src/pages/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function NotFound() {
-  return (
-    <Typography variant="h2" fontWeight={"bold"} style={{ marginTop: "150px" }}>
-      404 Page Not Found
-    </Typography>
-  );
-}
