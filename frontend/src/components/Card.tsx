768dd93c3b54072635825fa745b87c9b40accbdc
Sara Seeberg-Rommetveit <sarasee@stud.ntnu.no>
2024-02-15 12:00:29 +0100

    Enhance: pull out css, add q/a flip
    Co-authored-by: amaliejvik
    
    Related to #16

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -13,43 +13,67 @@ const Card: React.FC<CardProps> = ({ question, answer, isFlipped }) => {
         width: "40em",
         padding: "2em",
         display: "flex",
-        justifyContent: "center",
         background: "#AC94F4",
         maxWidth: "40em",
+        borderRadius: "2em",
       }}
     >
       <div
-        className="card"
         style={{
-          maxHeight: "20em",
-          maxWidth: "40em",
           display: "flex",
-          alignItems: "center",
+          flexDirection: "column",
+          width: "40em",
+          gap: "2em",
+          height: "100%",
         }}
       >
-        {isFlipped ? (
-          <div
-            className="answer"
-            style={{
-              fontSize: "2em",
-              overflowWrap: "break-word",
-              wordBreak: "break-word",
-            }}
-          >
-            {answer}
-          </div>
-        ) : (
-          <div
-            className="question"
-            style={{
-              fontSize: "2em",
-              overflowWrap: "break-word",
-              wordBreak: "break-word",
-            }}
-          >
-            {question}
-          </div>
-        )}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          {isFlipped ? <p> Answer</p> : <p> Question </p>}
+          <p> Difficulty</p>
+        </div>
+
+        <div
+          className="card"
+          style={{
+            maxHeight: "20em",
+            maxWidth: "40em",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {isFlipped ? (
+            <div
+              className="answer"
+              style={{
+                fontSize: "2em",
+                overflowWrap: "break-word",
+                wordBreak: "break-word",
+                height: "100%",
+              }}
+            >
+              {answer}
+            </div>
+          ) : (
+            <div
+              className="question"
+              style={{
+                fontSize: "2em",
+                overflowWrap: "break-word",
+                wordBreak: "break-word",
+                height: "100%",
+              }}
+            >
+              {question}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/ViewCardsPage.tsx
+++ frontend/src/pages/ViewCardsPage.tsx
@@ -3,6 +3,7 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Card from "../components/Card";
 import { useState } from "react";
+import styles from "./ViewCardsStyle.module.css";
 
 export default function ViewCards() {
   const [flipped, setFlipped] = useState(false);
@@ -12,45 +13,26 @@ export default function ViewCards() {
   };
   return (
     <div>
-      <div style={{ padding: "2em" }}>
+      <div id={styles.outerDiv}>
         <h2>Title of learningSet</h2>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            alignItems: "center",
-          }}
-        >
-          <div>
-            <Card
-              question={"Hvem er den beste PU gruppa?"}
-              answer={"Gruppe 50"}
-              isFlipped={flipped}
-            ></Card>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              width: "100%",
-              padding: "1em",
-              maxWidth: "20em",
-            }}
-          >
+        <div id={styles.innerDiv}>
+          <Card
+            question={
+              "Hvem er den beste PU gruppa i hele verden overalt på jord? "
+            }
+            answer={"Gruppe 50"}
+            isFlipped={flipped}
+          ></Card>
+          <div id={styles.flipButtonDiv}>
             <Button>
               <ArrowBackIcon />
             </Button>
             <Button
+              id={styles.flipButton}
               onClick={handleFlipButtonClick}
               type="button"
               fullWidth
               variant="contained"
-              style={{
-                background: "#d3d3d3",
-                opacity: "0.5",
-                color: "black",
-              }}
             >
               Flip
             </Button>
@@ -58,23 +40,12 @@ export default function ViewCards() {
               <ArrowForwardIcon />
             </Button>
           </div>
-          <div
-            style={{
-              width: "100%",
-              padding: "1em",
-              maxWidth: "10em",
-            }}
-          >
+          <div id={styles.backButtonDiv}>
             <Button
+              id={styles.backButton}
               type="button"
               fullWidth
               variant="contained"
-              style={{
-                background: "#d3d3d3",
-                maxWidth: "20em",
-                opacity: "0.5",
-                color: "black",
-              }}
             >
               Back
             </Button>
