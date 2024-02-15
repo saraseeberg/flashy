d5765068b31e004bc00256729f9f8346c4d01249
Kaveen <kaveeng@stud.ntnu.no>
2024-02-15 15:56:13 +0100

    feat: Edit favorite icons
    - update imports for staricon to mark as favorite
    
    Related to #2

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
+import EditIcon from '@mui/icons-material/Edit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons'; 
+import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons';
 
 interface LearningSet {
   id: number;
@@ -30,6 +34,11 @@ export default function Dashboard() {
     navigate("/create-set");
   };
 
+  const handleEditSets = () => {
+    navigate("/edit-set")
+
+  };
+
   const handleSetFavorite = (id: number) => {
     console.log("Set favorite", id);
   };
@@ -55,6 +64,13 @@ export default function Dashboard() {
         >
           Create new learning set
         </Button>
+        <Button
+          variant="outlined"
+          startIcon={<EditIcon />}
+          onClick={handleEditSets}
+        >
+          Edit learning set
+        </Button>
       </div>
       <Box>
         <Grid container spacing={2} sx={{ overflowY: "scroll" }}>
@@ -72,6 +88,9 @@ export default function Dashboard() {
                   maxWidth: "300px",
                   margin: " auto",
                   position: "relative",
+                  '&:hover': {
+                    transform: 'scale(1.05)', 
+                  }
                 }}
               >
                 <Button
@@ -84,9 +103,11 @@ export default function Dashboard() {
                   }}
                 >
                   {learningSet.isFavorite ? (
-                    <StarBorderPurple500Icon style={{ color: "gold" }} />
+                    // <StarBorderPurple500Icon style={{ color: "gold" }} />
+                    <FontAwesomeIcon icon={fasFaStar} color="gold" />
                   ) : (
-                    <StarBorderIcon style={{ color: "black" }} />
+                    // <StarBorderIcon style={{ color: "black" }} />
+                    <FontAwesomeIcon icon={farFaStar} color="black" />
                   )}
                 </Button>
                 <Typography variant="h6">{learningSet.name}</Typography>
