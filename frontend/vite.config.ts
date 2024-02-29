2535b5bb34aa4f608542c55deaf8291a7420cac6
Christian Andersen <chrijand@stud.ntnu.no>
2024-02-29 15:00:05 +0100

    test: Set up enviroment
    - Install packages and setup-file for testing.
    
    Related to #9

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { UserConfigExport, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    testMatch: ["**/*.test.tsx"],
+    css: true,
+  },
+} as UserConfigExport);
