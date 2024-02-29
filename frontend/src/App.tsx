80ff86683bea04d4c6ebed6b1cb24d74d0465f85
Christian Andersen <chrijand@stud.ntnu.no>
2024-02-29 21:45:20 +0100

    refactor: Edit routing for secure paths
    - Rewrite of AppRouter, App and main for secure paths and better readability.
    - Extracted theme in own file and wrapped outside of application with ThemeProvider.
    
    Related to #26

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,21 @@
-import AppRouter from "./AppRouter";
-import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#9F70FD",
-      dark: '#7F27FF',
-    },
-    secondary: {
-      main: "#FDBF60",
-      dark: "#FF8911",
-    },
-  }
-
-});
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import TopBar from "./components/TopBar";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <AppRouter />
-      </AuthProvider>
-    </ThemeProvider>
+    <Box sx={{ height: "100vh", width: "100vw", overflowX: "hidden" }}>
+      <TopBar />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
   );
 }
 
