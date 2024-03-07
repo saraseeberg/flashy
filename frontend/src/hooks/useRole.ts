f4d72f658f6321d1c9f01c6949eef48ad249beca
amaliejvik <amaligjo@stud.ntnu.no>
2024-03-07 14:16:56 +0100

    fix: create userRole hook and make AdminPage-button invisible for regular users
    Closes #36

---
+++ frontend/src/hooks/useRole.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { auth, db } from "../config/firebase";
+import { Firestore, doc, getDoc } from "firebase/firestore";
+
+function useUserRole() {
+  const { isAuthenticated } = useAuth();
+  const currentUserId = auth.currentUser?.uid;
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      if (currentUserId) {
+        const userDocRef = doc(db as Firestore, "usersData", currentUserId);
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        if (userDocSnapshot.exists()) {
+          const userData = userDocSnapshot.data();
+          const role = userData?.role;
+          setUserRole(role);
+        } else {
+          console.error("User document not found.");
+        }
+      }
+    };
+
+    if (isAuthenticated && currentUserId) {
+      fetchUserRole();
+    }
+  }, [isAuthenticated, currentUserId]);
+
+  return userRole;
+}
+
+export default useUserRole;
