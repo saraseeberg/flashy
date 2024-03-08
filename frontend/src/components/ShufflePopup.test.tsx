059f90a0b4c2947e211ed4d3739255fabea65f92
amaliejvik <amaligjo@stud.ntnu.no>
2024-03-08 16:20:15 +0100

    test: test for different popup components

---
+++ frontend/src/components/ShufflePopup.test.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import ShufflePopup from "./ShufflePopup";
+
+test("ShufflePopup renders correctly and handles events", async () => {
+  const onShuffle = vi.fn();
+  const onNoShuffle = vi.fn();
+
+  // Render the component
+  render(
+    <ShufflePopup
+      open={true}
+      onClose={vi.fn()}
+      onShuffle={onShuffle}
+      onNoShuffle={onNoShuffle}
+    />
+  );
+
+  expect(
+    screen.getByText("Do you want the cards to be shuffled?")
+  ).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText("No shuffle!"));
+  expect(onNoShuffle).toHaveBeenCalled();
+
+  fireEvent.click(screen.getByText("Shuffle!"));
+  expect(onShuffle).toHaveBeenCalled();
+});
