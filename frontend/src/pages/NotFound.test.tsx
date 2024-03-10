fc114503a41e6c362600848e088f0ff207d5e9cd
Sara Seeberg-Rommetveit <sarasee@stud.ntnu.no>
2024-03-10 13:59:40 +0100

    feature: make test for not found page

---
+++ frontend/src/pages/NotFound.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import NotFound from "./NotFoundPage";
+
+describe("NotFound Page", () => {
+  it("displays the 'Page not found' and additional guidance correctly", () => {
+    render(<NotFound />);
+
+    expect(screen.getByText("Page not found")).toBeInTheDocument();
+
+    expect(
+      screen.getByText(
+        "The path you have specified does not exist. Please press on the app-logo to return back to the main-page or specify a valid path."
+      )
+    ).toBeInTheDocument();
+  });
+});
